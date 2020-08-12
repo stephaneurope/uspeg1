55f6e3d6f4eef51eb9f534625d8c544d4299f249
serri stephan <serri.stephan@gmail.com>
2020-08-12 17:47:43 +0200

    mis en place du DQL

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,10 +16,12 @@ class DashboardController extends AbstractController
     /**
      * @Route("admin/dashboard", name="dashboard")
      */
-    public function index()
+    public function index(ObjectManager $manager)
     {
+        $adherents = $manager->createQuery('SELECT COUNT(a) FROM App\Entity\Adherent a')->getSingleScalarResult();
+        $produits = $manager->createQuery('SELECT COUNT(p) FROM App\Entity\Produit p')->getSingleScalarResult();
         return $this->render('admin/dashboard/index.html.twig', [
-            
+            'stats' => compact('adherents','produits')
         ]);
     }
 }
