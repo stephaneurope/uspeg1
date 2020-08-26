08d1b30ca15c37cb3be7da0d9120d04fd95e6179
serri stephan <serri.stephan@gmail.com>
2020-08-26 20:23:32 +0200

    mis a jour

--- src/Controller/AdminCommandeController.php
+++ src/Controller/AdminCommandeController.php
@@ -2,13 +2,14 @@
 namespace App\Controller;
 
 
-use Doctrine\Persistence\ObjectManager;
 use App\Entity\Commande;
 use App\Repository\CommandeRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Validator\Constraints\Length;
 
 class AdminCommandeController extends AbstractController
 {
@@ -56,29 +57,33 @@ class AdminCommandeController extends AbstractController
     foreach($commande as $c){
        $produit= $c->getproduit()->getId();
        $dateattribution = $c->getdateattribution();
+       $code = $c->getproduit()->getcode();
        $title = $c->getproduit()->getTitle();
+      
        if ($dateattribution == null) {
           
      
       var_dump( $produit);  }
-
+      
+      
+      
       if ($dateattribution == null) {
-        $commandes[]= $title . ' '. $repo->Essai($produit); 
+        $commandes[]= $title.' | '.'code barre: '.$code.' | '.' Quantité manquante: '.$repo->Essai($produit); 
     }
     }
    
-      // $commandes = $manager->createQuery('SELECT c, p, sum(c.qte) as cumul  FROM \App\Entity\Commande c INNER JOIN \App\Entity\Produit p WITH  c.dateattribution  IS NULL  GROUP BY p.id ')->getScalarResult();
+       //$commandes = $manager->createQuery('SELECT c, p, sum(c.qte) as cumul  FROM \App\Entity\Commande c INNER JOIN \App\Entity\Produit p WITH  c.dateattribution  IS NULL  GROUP BY p.id')->getScalarResult();
       // $commandes = $manager->createQuery('SELECT p,c, sum(c.qte) as cumul FROM \App\Entity\Produit p INNER JOIN \App\Entity\Commande c WITH c.dateattribution IS NULL GROUP BY p.id')->getScalarResult();
 
-       /*$commandes = $manager->createQuery('SELECT adherentId,produitId, SUM(qte) as TOTAL_QTE FROM App\Entity\Commande GROUP BY adherentId, produitId')->getResult();
+      //$commandes = $manager->createQuery('SELECT adherentId,produitId, SUM(qte) as TOTAL_QTE FROM App\Entity\Commande GROUP BY adherentId, produitId')->getResult();
 
-$out = array();
+//$out = array();
 
-foreach($commandes as $C){
+/*foreach($commandes as $C){
  $out[$C['adherentId']][$C['produitId']] = $C['TOTAL_QTE'];
 
-}
-var_dump($out);*/
+}*/
+//var_dump($out);
     
         return $this->render('admin/commande/a-commandes.html.twig', [
         'commande' => $commande,
