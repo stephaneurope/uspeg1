8dfef1006f948bf021c6068c8f1f6c9d4f8c3001
serri stephan <serri.stephan@gmail.com>
2020-09-10 17:24:24 +0200

    possibilité d'imprimer la liste d a dherent

--- src/Controller/PrintController.php
+++ src/Controller/PrintController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use Dompdf\Dompdf;
 use Dompdf\Options;
+use App\Entity\Team;
+use App\Entity\Adherent;
 use App\Entity\Commande;
+use App\Entity\CategoryAdherent;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -32,46 +35,92 @@ class PrintController extends AbstractController
         // Configure Dompdf according to your needs
         $pdfOptions = new Options();
         $pdfOptions->set('defaultFont', 'Arial');
-        
+
         // Instantiate Dompdf with our options
         $dompdf = new Dompdf($pdfOptions);
+        
         $repo = $this->getDoctrine()->getRepository(Commande::class);
         $commande = $repo->findAll();
-    foreach($commande as $c){
-       $produit= $c->getproduit()->getId();
-       $dateattribution = $c->getdateattribution();
-       $code = $c->getproduit()->getcode();
-       $title = $c->getproduit()->getTitle();
-      
-       if ($dateattribution == null) {
-          
-     
-       }
-      
-      
-      
-      if ($dateattribution == null) {
-        $commandes[]= $title.' | '.'code barre: '.$code.' | '.' Quantité manquante: '.$repo->Essai($produit); 
-    }
-    }
-   
-       
-    
-  
-        
+        foreach ($commande as $c) {
+            $produit = $c->getproduit()->getId();
+            $dateattribution = $c->getdateattribution();
+            $code = $c->getproduit()->getcode();
+            $title = $c->getproduit()->getTitle();
+
+            if ($dateattribution == null) {
+            }
+
+
+
+            if ($dateattribution == null) {
+                $commandes[] = $title . ' | ' . 'code barre: ' . $code . ' | ' . ' Quantité manquante: ' . $repo->Essai($produit);
+            }
+        }
+
+
         // Retrieve the HTML generated in our twig file
         $html = $this->renderView('print/commande_en_cours.html.twig', [
             'commande' => $commande,
-               'commandes' => $commandes
-             
-    
-            ]);
-        
+            'commandes' => $commandes
+
+
+        ]);
+
         // Load HTML to Dompdf essai
         $dompdf->loadHtml($html);
-        
+
         // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
         $dompdf->setPaper('A4', 'portrait');
+
+        // Render the HTML as PDF
+        $dompdf->render();
+
+        // Output the generated PDF to Browser (force download)
+        $dompdf->stream("Articles_manquants.pdf", [
+            "Attachment" => true
+        ]);
+    }
+
+/**
+     * Imprimer la liste des adhérents par catégorie
+     * 
+     * 
+     * @Route("print/{id}/category_adherent", name="print_category_adherent")
+     */
+    public function categoryAdherent($id)
+    {
+        // Configure Dompdf according to your needs
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+
+        // Instantiate Dompdf with our options
+        $dompdf = new Dompdf($pdfOptions);
+      
+        $repo = $this->getDoctrine()->getRepository(CategoryAdherent::class);
+        $categoryadherent = $repo->find($id);
+        $catadherent = $repo->findAll();
+
+        $repo1 = $this->getDoctrine()->getRepository(Adherent::class);
+        //$adherent = $repo1->findAll();
+        $adherent =$repo1->findBy([],['lastName' => 'ASC']);
+        $repo2 = $this->getDoctrine()->getRepository(Team::class);
+        $team1 = $repo2->findAll();
+
+         // Retrieve the HTML generated in our twig file
+         $html = $this->renderView('print/category_adherent.html.twig', [
+            'adherent' => $adherent,
+            'catadherent' => $catadherent,
+            'categoryadherent' => $categoryadherent,
+            'team1' => $team1
+        ]);
+        // Load HTML to Dompdf essai
+        $dompdf->loadHtml($html);
+     
+
+        // (Optional) Setup the paper size and orientation 'portrait' or 'portrait'
+        $dompdf->setPaper('A4', 'portrait');
+      
+     
 
         // Render the HTML as PDF
         $dompdf->render();
@@ -80,10 +129,11 @@ class PrintController extends AbstractController
         $dompdf->stream("mypdf.pdf", [
             "Attachment" => true
         ]);
+
+
     }
 
 
-       
-        
-    }
+
+}
 
