a6392adbea9322972be185607d5641f1e1312fee
serri stephan <serri.stephan@gmail.com>
2020-09-30 08:20:43 +0200

    permission de supression d utilisateur

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,5 +22,26 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'user' => $user,
         ]);
+    }
+
+    /**
+     * Permet de suprimer un user
+     * 
+     * @Route("user/{id}/delete", name="user_delete")
+     *
+     * @param ObjectManager $manager
+     * @param User $user
+     * @return Response
+     */
+    public function delete(ObjectManager $manager,User $user)
+    {
+      
+        $manager->remove($user);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            "L'utilisateur a bien été supprimé !");
+            return $this->redirectToRoute("account_utilisateur");
+        
     }
 }
